645415a9f7279d1344d7ac24b822983516a6d69f
bytesnail <lianghr1988@gmail.com>
2017-04-26 17:56:44 +0800

    移除drop方法, 优化代码, 更新README.md

--- README.md
+++ README.md
@@ -5,7 +5,11 @@
 ## API
 * abort 终止接受请求结果, 立即执行处理程序
 
-## TODO
+**todo**
 * hold 暂存请求结果, 不执行处理程序
 * exec 取消暂存状态, 执行处理程序
 
+
+## Static API
+
+* clear 调用所有fetch请求的abort方法, 清空缓存实例列表
--- src/index.js
+++ src/index.js
@@ -5,18 +5,28 @@ var fetch = require('isomorphic-fetch');
 // FetchExtend实例数组
 var FetchIns = []
 
-// fetch扩展类
+function FetchIns_Add(ins) {
+	FetchIns.push(ins)
+}
+
+function FetchIns_Delete(ins) {
+	var index = FetchIns.indexOf(ins)
+	FetchIns.splice(index, 1)
+	return ins
+}
+
+// Fetch扩展类
 function FetchExtend(url, opts) {
 	var _this = this
 	this._status = 'pending'
 	this._thenFn = []
 	this._fetch = fetch(url, opts).then(function(res){
 		_this._status === 'pending' && ( _this._status = 'resolved' )
-		Fetch.drop(_this)
+		FetchIns_Delete(_this)
 		return res
 	}, function(err){
 		_this._status === 'pending' && ( _this._status = 'rejected' )
-		Fetch.drop(_this)
+		FetchIns_Delete(_this)
 		return err
 	})
 }
@@ -49,20 +59,20 @@ FetchExtend.prototype.catch = function(catchFn) {
 FetchExtend.prototype.abort = function(res) {
 	if (this._status === 'pending') {
 		this._status = 'abort'
-		this._thenFn.reduce(function(p, fn){
+		FetchIns_Delete(this)
+		return this._thenFn.reduce(function(p, fn){
 			return p.then(...fn)
 		}, Promise.reject(res || 'abort'))
-		return Fetch.drop(this)
 	} else {
 		console.log('fetch实例已获取结果, 无法终止请求')
 	}
 }
 
-// 输出FetchExtend实例
+// 实例化FetchExtend扩展类
 function Fetch(url, opts) {
 	if (!url) return
 	var ins = new FetchExtend(url, opts || {})
-	FetchIns.push(ins)
+	FetchIns_Add(ins)
 	return ins
 }
 
@@ -71,11 +81,6 @@ Fetch.clear = function() {
 		fetch.abort()
 	})
 	FetchIns = []
-}
-
-Fetch.drop = function(ins) {
-	var index = FetchIns.indexOf(ins)
-	FetchIns.splice(index, 1)
 }
 
 module.exports = Fetch
